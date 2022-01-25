d944effa33ad9d51ad339e6de5d36fb4e2fc99ec
realzzce <zzce1603@gmail.com>
2022-01-25 10:37:02 +0800

    search recipe by tag

--- chapter2/main.go
+++ chapter2/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/rs/xid"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -68,22 +69,39 @@ func UpdateRecipeHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, recipe)
 }
 
-func DeleteRecipeHandler(c *gin.Context){
-		id := c.Param("id")
-		index := -1
-		for i := 0; i < len(recipes); i++ {
-			if recipes[i].ID == id {
-				index = i
+func DeleteRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+		}
+	}
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found"})
+		return
+	}
+	recipes = append(recipes[:index], recipes[index+1:]...)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted"})
+}
+
+func SearchRecipeHandler(c *gin.Context) {
+	tag := c.Query("tag")
+	listOfRecipes := make([]Recipe, 0)
+	for i := 0; i < len(recipes); i++ {
+		found := false
+		for _, t := range recipes[i].Tags {
+			if strings.EqualFold(t, tag) {
+				found = true
 			}
 		}
-		if index == -1 {
-			c.JSON(http.StatusNotFound, gin.H{
-				"error": "Recipe not found"})
-			return
+		if found {
+			listOfRecipes = append(listOfRecipes, recipes[i])
 		}
-		recipes = append(recipes[:index], recipes[index+1:]...)
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Recipe has been deleted"})
+	}
+	c.JSON(http.StatusOK, listOfRecipes)
 }
 
 func main() {
@@ -93,6 +111,7 @@ func main() {
 	router.GET("/recipes", ListRecipesHandler)
 	router.PUT("/recipes/:id", UpdateRecipeHandler)
 	router.DELETE("/recipes/:id", DeleteRecipeHandler)
+	router.GET("/recipes/search", SearchRecipeHandler)
 
 	router.Run()
 }
